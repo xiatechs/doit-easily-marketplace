9b628dee8c727da6ebd10f3ddd70ef86a9767b3b
Tim Kriel <tim.k@xiatech.co.uk>
2023-10-12 21:55:34 +0100

    DOI-140 Service Account

--- docs/terraform/app_deploy/providers.tf
+++ docs/terraform/app_deploy/providers.tf
@@ -2,11 +2,15 @@ provider "google-beta" {
   project = var.project_id
 }
 
+data "google_service_account" "doit-easily" {
+  account_id = "doit-easily"
+  project = var.project_id
+}
 
 #get an access token for the doit-easily SA
 data "google_service_account_access_token" "prod_token" {
-  # target_service_account = data.google_service_account.doit_easily_backend_integration_sa.email
-  target_service_account = local.service_account_email
+  # target_service_account = local.service_account_email
+  target_service_account = data.google_service_account.doit-easily.email
   scopes                 = ["userinfo-email", "cloud-platform"]
   lifetime               = "1200s"
 }
@@ -16,5 +20,3 @@ provider "google" {
   alias        = "prod_impersonation"
   access_token = data.google_service_account_access_token.prod_token.access_token
 }
-
-
