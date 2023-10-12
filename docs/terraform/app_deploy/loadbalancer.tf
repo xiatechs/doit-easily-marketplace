1ea6976e4bfe7d007b18dc155dd138b97d94bbb6
Tim Kriel <tim.k@xiatech.co.uk>
2023-10-12 21:20:46 +0100

    DOI-140 Added App Deploy

--- docs/terraform/app_deploy/loadbalancer.tf
+++ docs/terraform/app_deploy/loadbalancer.tf
@@ -11,6 +11,7 @@ resource "google_compute_global_address" "external_ip" {
 # Add the IP to the DNS
 resource "google_dns_record_set" "api" {
   name         = "${var.domain}."
+  project      = data.google_dns_managed_zone.dns_zone.project
   type         = "A"
   ttl          = 300
   managed_zone = data.google_dns_managed_zone.dns_zone.name
