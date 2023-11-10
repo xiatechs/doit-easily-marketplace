305249724663616e77f3cf1dd8e67a43305cf741
Tim Kriel <tim.k@xiatech.co.uk>
2023-11-10 15:42:51 +0000

    DOI-140 Marketplace admin variables update

--- docs/terraform/app_deploy/iap.tf
+++ docs/terraform/app_deploy/iap.tf
@@ -4,15 +4,15 @@ resource "google_project_service_identity" "iap_gsi" {
 }
 
 # This can be applied only once
-# resource "google_iap_brand" "iap_brand" {
-#   application_title = var.brand_name
-#   # The user/service account running the terraform has to have ownership over the email account in Google Groups.
-#   # To workaround this, if it's intended only for internal use, one may decide to use the email attached to that service account.
-#   support_email     = var.brand_support_email
-#   project           = var.project_id
-# }
+resource "google_iap_brand" "iap_brand" {
+  application_title = var.brand_name
+  # The user/service account running the terraform has to have ownership over the email account in Google Groups.
+  # To workaround this, if it's intended only for internal use, one may decide to use the email attached to that service account.
+  support_email     = var.brand_support_email
+  project           = var.project_id
+}
 
-# resource "google_iap_client" "iap_client" {
-#   display_name = var.iap_client_display_name
-#   brand        = google_iap_brand.iap_brand.name  
-# }
+resource "google_iap_client" "iap_client" {
+  display_name = var.iap_client_display_name
+  brand        = google_iap_brand.iap_brand.name  
+}
